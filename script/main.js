4708d10e0decce084e3adf0734f4d8120af1dfcd
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2023-10-11 00:36:04 +0800

    complete navbar design and function

--- script/main.js
+++ script/main.js
@@ -1,5 +1,18 @@
 // hamburger function
-const hamburger = document.querySelector(".menu-icon");
-hamburger.addEventListener("click", (event)=>{
-    hamburger.classList.toggle("change");
+const openHamburger = document.querySelector(".menu-icon");
+openHamburger.addEventListener("click", (event) => {
+    const menu = document.querySelector("div.menu");
+    menu.style.height = "100%";
 })
+const closeHamburger = document.querySelector(".close-icon");
+closeHamburger.addEventListener("click", (event) => {
+    const menu = document.querySelector("div.menu");
+    menu.style.height = "0%";
+    addClickToMenuItems();
+})
+
+function addClickToMenuItems() {
+    const menuItems = document.querySelectorAll("div.menu nav ul>li");
+    menuItems.forEach((menuItem) => menuItem.addEventListener("click", closeMenu));
+}
+
