db660b5edf22afca5c512a068817b8e8ff510ba2
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2023-10-12 03:04:27 +0800

    adding function that when link click in mobile, it will close but still incomplete

--- script/main.js
+++ script/main.js
@@ -1,19 +1,27 @@
-// hamburger function
-const openHamburger = document.querySelector(".menu-icon");
-openHamburger.addEventListener("click", (event) => {
-    const menu = document.querySelector("div.menu");
-    menu.style.height = "100%";
-})
-const closeHamburger = document.querySelector(".close-icon");
-closeHamburger.addEventListener("click", (event) => {
-    const menu = document.querySelector("div.menu");
-    menu.style.height = "0%";
-})
+document.addEventListener("DOMContentLoaded", function () {
+    // hamburger function
+    const openHamburger = document.querySelector(".menu-icon");
+    openHamburger.addEventListener("click", (event) => {
+        const menu = document.querySelector("div.menu");
+        menu.style.height = "100%";
+    })
+    const closeHamburger = document.querySelector(".close-icon");
+    closeHamburger.addEventListener("click", (event) => {
+        const menu = document.querySelector("div.menu");
+        menu.style.height = "0%";
+    })
 
-// function addClickToMenuItems() {
-const menuItems = document.querySelectorAll("div.menu nav ul>li");
-menuItems.forEach((menuItem) => menuItem.addEventListener("click", ()=>{
-    const menu = document.querySelector("div.menu");
-    menu.style.height = "0%";
-}));
+    // function addClickToMenuItems() {
+    function closeMenu() {
+        const menu = document.querySelector("div.menu");
+        menu.style.height = "0%";
+    }
 
+    // function to add click event to menu items
+    function addClickToMenuItems() {
+        const menuItems = document.querySelectorAll("header div.menu nav ul > li a");
+        menuItems.forEach((menuItem) => menuItem.addEventListener("click", closeMenu));
+    }
+
+    addClickToMenuItems();
+});
