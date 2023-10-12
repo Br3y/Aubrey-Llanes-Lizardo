5f77b960223309f6b326a42343c002cdebbdd1fd
Aubrey Lizardo <lizardoaubrey.software@gmail.com>
2023-10-12 09:34:55 +0800

    complete

--- script/main.js
+++ script/main.js
@@ -1,27 +1,38 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // hamburger function
-    const openHamburger = document.querySelector(".menu-icon");
-    openHamburger.addEventListener("click", (event) => {
-        const menu = document.querySelector("div.menu");
-        menu.style.height = "100%";
-    })
-    const closeHamburger = document.querySelector(".close-icon");
-    closeHamburger.addEventListener("click", (event) => {
-        const menu = document.querySelector("div.menu");
-        menu.style.height = "0%";
-    })
+// document.addEventListener("DOMContentLoaded", function () {
+//     // hamburger function
+//     // const openHamburger = document.querySelector(".menu-icon");
+//     // openHamburger.addEventListener("click", (event) => {
+//     //     const menu = document.querySelector("div.menu");
+//     //     menu.style.height = "100%";
+//     // })
+//     // const closeHamburger = document.querySelector(".close-icon");
+//     // closeHamburger.addEventListener("click", (event) => {
+//     //     const menu = document.querySelector("div.menu");
+//     //     menu.style.height = "0%";
+//     // })
 
-    // function addClickToMenuItems() {
-    function closeMenu() {
-        const menu = document.querySelector("div.menu");
-        menu.style.height = "0%";
+// });
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    function showMenu(){
+        const openHamburger = document.querySelector("div.menu");
+        openHamburger.style.height = "100%";
     }
 
-    // function to add click event to menu items
-    function addClickToMenuItems() {
-        const menuItems = document.querySelectorAll("header div.menu nav ul > li a");
-        menuItems.forEach((menuItem) => menuItem.addEventListener("click", closeMenu));
+    function closeMenu(){
+        const closeHamburger = document.querySelector("div.menu");
+        closeHamburger.style.height = "0%";
+        addClickToMenuItems()
     }
 
-    addClickToMenuItems();
-});
+    function addClickToMenuItems(){
+        const menuItems = document.querySelectorAll("div.menu nav ul>li a");
+        menuItems.forEach((menuItem) => menuItem.addEventListener("click", closeMenu))
+
+    }
+    
+    document.querySelector(".menu-icon").addEventListener("click", showMenu);
+    document.querySelector(".close-icon").addEventListener("click", closeMenu);
+    addClickToMenuItems()
+})
